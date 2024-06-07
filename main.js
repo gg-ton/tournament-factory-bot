25be21c4efd3917b9d0e69d105ae1ee6a4fab8be
Vitaliy <vitaliygosteev@gmail.com>
2024-06-07 12:52:18 +0400

    feat: add pinning of start message and sending additional message

---
+++ main.js
@@ -0,0 +1,36 @@
+import { Telegraf, Markup } from 'telegraf';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const token = process.env.BOT_TOKEN;
+const webkitURL = process.env.WEBKIT_URL;
+
+const bot = new Telegraf(token);
+
+bot.command('start', async ctx => {
+  const message = await ctx.reply(
+    'Welcome! Let me help you get started with the app. Click the button below to begin.',
+    Markup.inlineKeyboard([Markup.button.webApp('Launch the app', webkitURL)])
+  );
+
+  try {
+    await ctx.pinChatMessage(message.message_id);
+  } catch (error) {
+    console.error('Failed to pin the message:', error);
+  }
+
+  ctx.reply('Click here to launch the app 👇');
+});
+
+bot.command('help', ctx => {
+  const helpMessage = `
+  Here is the list of available commands:
+  /start - Start the bot
+  /help - Show the list of commands
+  `;
+
+  ctx.reply(helpMessage);
+});
+
+bot.launch();
